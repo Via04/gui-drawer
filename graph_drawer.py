008a548ae8ac7dfe75a559ff6a4d29b4334ede24
Ivan Vashchenko <Via0408@gmail.com>
2021-10-19 12:10:57 +0300

    added graph window, enhanced parse engine

--- graph_drawer.py
+++ graph_drawer.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.figure
 from re import findall
 import numpy as np  # do not change np as it used as pattern addon
 
@@ -11,36 +12,56 @@ import numpy as np  # do not change np as it used as pattern addon
 class PlotHelper:
     param_marker = ''
     val_marker = ''
+    usr_func_marker = ''
     min_x = 0
     max_x = 0
     complexity = 0
+    x = None
 
-    def __init__(self, min_x: float, max_x: float, complexity=100, param_marker='p_', val_marker='v_'):
+    def __init__(self, min_x=0, max_x=0, complexity=100, param_marker='p_', val_marker='v_',
+                 usr_func_marker='u_'):
         self.param_marker = param_marker
         self.val_marker = val_marker
+        self.usr_func_marker = usr_func_marker
         self.min_x = min_x
         self.max_x = max_x
         self.complexity = complexity
+        self.x = np.linspace(min_x, max_x, complexity)
 
-    def plot_expr(self, expr: str):
-        command = self.parse_expression(expr)
-        x = np.linspace(self.min_x, self.max_x, 100)
-        y = None
-        command = 'y = ' + command  # if you change y name variable, also change this line
-        py_code = compile(command, '<string>', 'exec')
+    def ans_expr(self, expr: str) -> list:
+        code = self.gen_pycode(expr)
+        x = self.x
         _local = locals()
-        exec(py_code, globals(), _local)
+        exec(code, globals(), _local)
         y = _local['y']
-        plt.plot(x, y, 'ro-')
-        plt.show()
+        return y
+
+    def gen_pycode(self, expr: str):
+        command = self.parse_expression(expr)
+        y = None
+        x = self.x
+        # command = 'y = ' + command  # if you change y name variable, also change this line
+        py_code = compile(command, '<string>', 'exec')
+        return py_code
+
+    def save_to_file(self, expr: str, filename):
+        expr = self.parse_expression(expr)
+        with open(filename, 'w') as f:
+            f.write(expr)
+
+    def read_from_file(self, filename):
+        with open(filename, 'rb') as f:
+            code = '\n'.join(f.readlines())
+            return code
 
     def parse_expression(self, expr: str) -> str:
         if expr is not None:
-            op_pattern = '\s[^' + self.param_marker[:1] + self.val_marker[:1] + '\W\d][^' + self.param_marker + \
-                      self.val_marker + ']?\w*\s'
+            op_pattern = '\\s[A-XZa-xz]+\\s'
+            #  '\\s[^' + self.param_marker[:1] + self.val_marker[:1] + self.usr_func_marker[:1] + 'y' + \
+                          #  '\\W\\d][^' + self.param_marker + self.val_marker + self.usr_func_marker + ']?\\w*\\s'
             # op_pattern = '\s[^pv\W][^__]?\w*\s'
-            val_pattern = '\s' + self.val_marker + '\w*\s'
-            param_pattern = '\s' + self.param_marker + '\w*\s'
+            val_pattern = '\\s' + self.val_marker + '\\w*\\s'
+            param_pattern = '\\s' + self.param_marker + '\\w*\\s'
             ops = findall(op_pattern, expr)
             vals = findall(val_pattern, expr)
             params = findall(param_pattern, expr)
@@ -48,12 +69,33 @@ class PlotHelper:
                 new_op = op[:1] + 'np.' + op[1:]
                 expr = expr.replace(op, new_op)
             for val in vals:
-                new_val = val[3:]
+                new_val = ' ' + val[3:-1] + ' '
                 expr = expr.replace(val, new_val)
             for param in params:
-                new_param = param[3:]
+                new_param = ' ' + param[3:-1] + ' '
                 expr = expr.replace(param, new_param)
-            return expr
+            expr_without_space = ''
+            for line in expr:
+                if line[0:1] == ' ':
+                    expr_without_space += line[1:]
+                else:
+                    expr_without_space += line
+            return expr_without_space
+
+    def plot_expr(self, expr: str):
+        y = self.ans_expr(expr)
+        x = self.x
+        plt.plot(x, y, 'ro-')
+        plt.show()
+
+    def get_figure(self, expr):
+        y = self.ans_expr(expr)
+        x = self.x
+        fig = plt.figure(figsize=(5, 4))
+        ax = fig.add_subplot(1, 1, 1)
+        ax.plot(x, y, (self.max_x - self.min_x) / self.complexity)
+        return fig
+
 
     @classmethod
     def plot_array(cls, x_list, y_list):
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import PySimpleGUI as gui
+from os.path import curdir
 
 from xlsx_parser import XLSXParser
 from graph_drawer import PlotHelper
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
 
 
 layout = [[gui.In(key='input-file'), gui.FileBrowse(button_text='Выберите xlsx файл', file_types=(('Excel files', '*.xlsx'),), target='input-file')],
@@ -16,17 +25,28 @@ while True:
         x_list = xlsx.get_column_values(1)
         y_list = xlsx.get_column_values(2)
         PlotHelper.plot_array(x_list, y_list)
-        break
     if event == 'Создание модели':
-        model_layout = [[gui.In(key='-MODEL-'), gui.Button('Create', bind_return_key=True, visible=False)]]
+        model_layout = [[gui.Multiline(key='-MODEL-', size=(100, 30)), gui.Button('Create', visible=True)],
+                        [gui.InputText(key='-SAVE-MOD-', enable_events=True, visible=False),
+                         gui.FileSaveAs('Save', target='-SAVE-MOD-', file_types=(('PYDATA', '.pydat'),),
+                                        initial_folder=curdir)]]
         mod_window = gui.Window('Создайте модель', model_layout)
         while True:
             mod_evt, mod_val = mod_window.read()
             if mod_evt == 'Create':
                 plot = PlotHelper(0, 100, 100)
                 model = mod_val['-MODEL-']
-                plot.plot_expr(model)
-                break
+                plot_layout = [[gui.Text('Model')], [gui.Canvas(key='-CANVAS-')]]
+                fig = plot.get_figure(model)
+                canv_window = gui.Window('Graph from model', plot_layout, finalize=True,
+                                    element_justification='center', font='Monospace 18')
+                fig_agg = draw_figure(canv_window['-CANVAS-'].TKCanvas, fig)
+                fig_agg.draw()
+            if mod_evt == '-SAVE-MOD-':
+                plot = PlotHelper()
+                model = mod_val['-MODEL-']
+                filename = mod_val['-SAVE-MOD-']
+                plot.save_to_file(model, filename)
             if mod_evt == gui.WINDOW_CLOSED:
                 break
         break
